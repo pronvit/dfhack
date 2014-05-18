02ef966c945cf874f010dcce4133ee46b9930ead
lethosor <lethosor@gmail.com>
2014-05-18 16:47:04 -0400

    Rewrite to use EmbarkTool struct, display label on embark screen

--- plugins/embark-tools.cpp
+++ plugins/embark-tools.cpp
@@ -14,10 +14,25 @@
 
 using namespace DFHack;
 
-std::map<std::string, bool> enabled_tools;
-std::map<std::string, std::string> tool_names;
+struct EmbarkTool
+{
+    std::string id;
+    std::string name;
+    std::string desc;
+    bool enabled;
+};
 
-command_result embark_tools (color_ostream &out, std::vector <std::string> & parameters);
+EmbarkTool embark_tools[] = {
+    {"anywhere", "Embark anywhere", "Allows embarking anywhere on the world map", false},
+    {"nano", "Nano embark", "Allows the embark size to be decreased below 2x2", false},
+};
+#define NUM_TOOLS sizeof(embark_tools) / sizeof(EmbarkTool)
+
+command_result embark_tools_cmd (color_ostream &out, std::vector <std::string> & parameters);
+
+bool tool_exists (std::string tool_name);
+bool tool_enabled (std::string tool_name);
+bool tool_enable (std::string tool_name, bool enable_state);
 
 /*
  * Viewscreen hooks
@@ -44,32 +59,100 @@ struct choose_start_site_hook : df::viewscreen_choose_start_sitest
         auto dim = Screen::getWindowSize();
         int x = 1,
             y = dim.y - 5;
-        OutputString(COLOR_LIGHTMAGENTA, x, y, "Enabled:");
+        OutputString(COLOR_LIGHTMAGENTA, x, y, "Enabled: ");
+        std::list<std::string> tools;
+        for (int i = 0; i < NUM_TOOLS; i++)
+        {
+            if (embark_tools[i].enabled)
+            {
+                tools.push_back(embark_tools[i].name);
+                tools.push_back(", ");
+            }
+        }
+        if (tools.size())
+        {
+            tools.pop_back();  // Remove last ,
+            for (auto iter = tools.begin(); iter != tools.end(); iter++)
+            {
+                OutputString(COLOR_LIGHTMAGENTA, x, y, *iter);
+            }
+        }
+        else
+        {
+            OutputString(COLOR_LIGHTMAGENTA, x, y, "(none)");
+        }
+
+        if (tool_enabled("anywhere"))
+        {
+            x = 20; y = dim.y - 2;
+            OutputString(COLOR_WHITE, x, y, ": Embark!");
+        }
     }
 };
 
 IMPLEMENT_VMETHOD_INTERPOSE(choose_start_site_hook, feed);
 IMPLEMENT_VMETHOD_INTERPOSE(choose_start_site_hook, render);
 
+/*
+ * Tool management
+ */
+
+bool tool_exists (std::string tool_name)
+{
+    for (int i = 0; i < NUM_TOOLS; i++)
+    {
+        if (embark_tools[i].id == tool_name)
+            return true;
+    }
+    return false;
+}
+
+bool tool_enabled (std::string tool_name)
+{
+    for (int i = 0; i < NUM_TOOLS; i++)
+    {
+        if (embark_tools[i].id == tool_name)
+            return embark_tools[i].enabled;
+    }
+    return false;
+}
+
+bool tool_enable (std::string tool_name, bool enable_state)
+{
+    for (int i = 0; i < NUM_TOOLS; i++)
+    {
+        if (embark_tools[i].id == tool_name)
+        {
+            embark_tools[i].enabled = enable_state;
+            return true;
+        }
+    }
+    return false;
+}
+
+/*
+ * Plugin management
+ */
+
 DFHACK_PLUGIN("embark-tools");
 DFHACK_PLUGIN_IS_ENABLED(is_enabled);
 
 DFhackCExport command_result plugin_init (color_ostream &out, std::vector <PluginCommand> &commands)
 {
+    std::string help = "";
+    help += "embark-tools (enable/disable) tool [tool...]\n"
+            "Tools:\n";
+    for (int i = 0; i < NUM_TOOLS; i++)
+    {
+        help += ("  " + embark_tools[i].id + ": " + embark_tools[i].desc + "\n");
+    }
     commands.push_back(PluginCommand(
         "embark-tools",
         "A collection of embark tools",
-        embark_tools,
+        embark_tools_cmd,
         false,
-        "embark-tools (enable/disable) tool [tool...]\n"
-        "Tools:\n"
-        "  anywhere: Allows embarking anywhere\n"
-        "  nano: Allows the embark size to be decreased below 2x2\n"
+        help.c_str()
     ));
-    enabled_tools["anywhere"] = false;
-    enabled_tools["nano"] = false;
-    tool_names["anywhere"] = "Embark anywhere";
-    tool_names["nano"] = "Nano embark";
     return CR_OK;
 }
 
@@ -92,7 +175,7 @@ DFhackCExport command_result plugin_enable (color_ostream &out, bool enable)
     return CR_OK;
 }
 
-command_result embark_tools (color_ostream &out, std::vector <std::string> & parameters)
+command_result embark_tools_cmd (color_ostream &out, std::vector <std::string> & parameters)
 {
     CoreSuspender suspend;
     if (parameters.size())
@@ -108,11 +191,9 @@ command_result embark_tools (color_ostream &out, std::vector <std::string> & par
             }
             else if (parameters[i] == "disable")
                 enable_state = false;
-            else if (enabled_tools.count(parameters[i]))
+            else if (tool_exists(parameters[i]))
             {
-                enabled_tools[parameters[i]] = enable_state;
-                out << (enable_state ? "Enabled" : "Disabled") << " " <<
-                    tool_names[parameters[i]] << std::endl;
+                tool_enable(parameters[i], enable_state);
             }
             else
                 return CR_WRONG_USAGE;
@@ -120,11 +201,18 @@ command_result embark_tools (color_ostream &out, std::vector <std::string> & par
     }
     else
     {
-        out << "Tool status:" << std::endl;
-        for (auto iter = enabled_tools.begin(); iter != enabled_tools.end(); iter++)
+        if (is_enabled)
         {
-            out << tool_names[iter->first] << " (" << iter->first << ")";
-            out << ": " << (iter->second ? "Enabled" : "Disabled") << std::endl;
+            out << "Tool status:" << std::endl;
+            for (int i = 0; i < NUM_TOOLS; i++)
+            {
+                EmbarkTool t = embark_tools[i];
+                out << t.name << " (" << t.id << "): " << (t.enabled ? "Enabled" : "Disabled") << std::endl;
+            }
+        }
+        else
+        {
+            out << "Plugin not enabled" << std::endl;
         }
     }
     return CR_OK;
