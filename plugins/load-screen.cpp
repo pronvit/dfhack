7fb352e86dc9f8a38e961d4387a08244ca52d256
lethosor <lethosor@gmail.com>
2014-05-20 19:13:52 -0400

    List savegames

--- plugins/load-screen.cpp
+++ plugins/load-screen.cpp
@@ -6,6 +6,7 @@
 
 #include <set>
 
+#include <modules/Gui.h>
 #include <modules/Screen.h>
 #include <VTableInterpose.h>
 
@@ -18,7 +19,24 @@ using namespace DFHack;
 
 using df::global::gps;
 
+std::string mode_string (df::game_type gametype);
+
 command_result cmd_load_screen (color_ostream &out, std::vector <std::string> & parameters);
+
+std::string mode_string (df::game_type gametype)
+{
+    switch (gametype)
+    {
+        case df::game_type::DWARF_MAIN:
+            return "Fortress mode";
+        case df::game_type::DWARF_RECLAIM:
+            return "Reclaim fortress mode";
+        case df::game_type::ADVENTURE_MAIN:
+            return "Adventure mode";
+        default:
+            return "";
+    }
+}
 
 class viewscreen_load_screen : public dfhack_viewscreen
 {
@@ -27,9 +45,25 @@ public:
     void render ();
     void help () { };
     std::string getFocusString() { return "loadscreen"; };
-    viewscreen_load_screen () { };
+    viewscreen_load_screen ();
     ~viewscreen_load_screen () { };
+    df::viewscreen_loadgamest* parent_screen ();
+protected:
+    int sel_idx;
+    std::vector<df::viewscreen_loadgamest::T_saves> saves;
 };
+
+viewscreen_load_screen::viewscreen_load_screen ()
+{
+    sel_idx = 0;
+}
+
+df::viewscreen_loadgamest* viewscreen_load_screen::parent_screen ()
+{
+    df::viewscreen * top = this->parent;
+    VIRTUAL_CAST_VAR(parent_screen, df::viewscreen_loadgamest, top);
+    return parent_screen;
+}
 
 void viewscreen_load_screen::feed (std::set<df::interface_key> *input)
 {
@@ -47,8 +81,26 @@ void viewscreen_load_screen::feed (std::set<df::interface_key> *input)
 
 void viewscreen_load_screen::render ()
 {
+    auto dim = Screen::getWindowSize();
+    df::viewscreen_loadgamest* parent = parent_screen();
+    if (!parent)
+        return;
     Screen::clear();
-    Screen::paintString(Screen::Pen(' ', COLOR_RED, COLOR_BLUE), 0, 0, "Load menu");
+    std::string title = "Load game (DFHack)";
+    Screen::paintString(Screen::Pen(' ', COLOR_WHITE, COLOR_BLACK),
+                        (dim.x / 2) - (title.length() / 2),
+                        0,
+                        title);
+    auto games = parent->saves;
+    int row = 2, i = 0;
+    for (auto iter = games.begin(); iter != games.end() && row + 2 < dim.y; iter++, row += 2, i++)
+    {
+        df::viewscreen_loadgamest::T_saves * save = *iter;
+        color_value fg = (i == sel_idx) ? COLOR_WHITE : COLOR_GREY;
+        auto pen = Screen::Pen(' ', fg, COLOR_BLACK);
+        Screen::paintString(pen, 2, row, save->fort_name + " - " + mode_string(save->game_type));
+        Screen::paintString(pen, 3, row + 1, "Folder: " + save->folder_name);
+    }
 }
 
 struct loadgame_hooks : df::viewscreen_loadgamest
